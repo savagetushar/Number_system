b538a236be785f0ee6e0a8fce5566357213d06a6
savagetushar <pandeytushar287@gmail.com>
2024-10-07 13:42:51 +0530

    Third commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const numbers=[19,23,4,16,28,13,31,8,29,14,6,35,2,11,17,5,9,27,12,30];
+const max=Math.max(...numbers);
+const min=Math.min(...numbers);
 function isPrime(num){
     if(num<=1)
         return false;
@@ -15,3 +17,5 @@ for(let i=0;i<numbers.length;i++){
     }
 }
 console.log("prime numbers",primeNumber);
+console.log("maximum value",max);
+console.log("minimum value",min);
